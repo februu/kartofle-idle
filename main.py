69f15d181f970d853d740f22a43302d81210f0fb
febru <81876955+februu@users.noreply.github.com>
2026-05-08 02:18:18 +0200

    Init python project with discordpy

---
+++ main.py
@@ -0,0 +1,25 @@
+import discord
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+intents = discord.Intents.all()
+client = discord.Client(intents=intents)
+
+
+@client.event
+async def on_ready():
+    print(f"We have logged in as {client.user}")
+
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith("$hello"):
+        await message.channel.send("Hello!")
+
+
+client.run(os.getenv("TOKEN", ""))
