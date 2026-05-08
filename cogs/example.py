c7ac16bfb7636aefb7d55fcb97583ce3fb4921f9
febru <81876955+februu@users.noreply.github.com>
2026-05-08 19:18:46 +0200

    Update example cog
    Co-Authored-By: DragoonXVIII <99823997+DragoonXVIII@users.noreply.github.com>

--- cogs/example.py
+++ cogs/example.py
@@ -2,14 +2,19 @@ import discord
 from discord.ext import commands
 from discord import app_commands
 
+import db
+
 
 class MyCog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @app_commands.command(name="hello", description="Says hello")
+    @app_commands.command(name="balance", description="Checks your balance")
     async def hello(self, interaction: discord.Interaction):
-        await interaction.response.send_message("Hello!")
+        balance = db.get_user_balance(interaction.user.id)
+        await interaction.response.send_message(
+            f"Siema, masz na koncie {balance} kartofli."
+        )
 
 
 async def setup(bot: commands.Bot):
