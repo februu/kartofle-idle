ec07f767f63f5b41e359079372d70748e9609513
febru <81876955+februu@users.noreply.github.com>
2026-05-09 02:58:34 +0200

    Convert Example cog to Economy cog

--- cogs/economy.py
+++ cogs/economy.py
@@ -5,17 +5,15 @@ from discord import app_commands
 import db
 
 
-class MyCog(commands.Cog):
+class Economy(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
     @app_commands.command(name="balance", description="Checks your balance")
-    async def hello(self, interaction: discord.Interaction):
+    async def balance(self, interaction: discord.Interaction):
         balance = db.get_user_balance(interaction.user.id)
-        await interaction.response.send_message(
-            f"Siema, masz na koncie {balance} kartofli."
-        )
+        await interaction.response.send_message(f"Siema, masz na koncie {balance} kartofli.")
 
 
 async def setup(bot: commands.Bot):
-    await bot.add_cog(MyCog(bot))
+    await bot.add_cog(Economy(bot))
