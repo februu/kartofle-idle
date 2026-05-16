7635c35d94248d1ffa6e90c82ff2b3053815a5f0
DragoonXVIII <99823997+DragoonXVIII@users.noreply.github.com>
2026-05-16 13:36:07 +0200

    Refactor checks.py
    Removed obsolete code

--- utils/checks.py
+++ utils/checks.py
@@ -5,17 +5,6 @@ from discord import app_commands
 _ADMINS = [int(x) for x in os.getenv("ADMIN_IDS", "").split(",")]
 
 
-# NOTE: Remove those redundant checks below, use decotators instead
-def is_admin(user: discord.User | discord.Member) -> bool:
-    """Checks if the user is an admin based on their ID."""
-    return user.id in _ADMINS
-
-
-def is_sent_in_guild(interaction: discord.Interaction) -> bool:
-    """Checks if the interaction was sent in a guild."""
-    return interaction.guild_id is not None
-
-
 def admin_only():
     """A decorator that checks if the user is an admin."""
 
